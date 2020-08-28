b554732a38f14a58b9ce295ce1c7b8c5f2c6402f
santhosh <rksginbox@gmail.com>
2020-08-28 02:53:50 +0100

    Updates

---
+++ precache-manifest.8c5e0bf2eac555ef6590b34c9b95a0e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3733400c82daf315a3175e9dd3ad61",
+    "url": "/rk-and-santosh/index.html"
+  },
+  {
+    "revision": "07c42ec5f5db3a44240f",
+    "url": "/rk-and-santosh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "579813e9f7628af3dfe4",
+    "url": "/rk-and-santosh/static/js/2.7ed6b3b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rk-and-santosh/static/js/2.7ed6b3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c42ec5f5db3a44240f",
+    "url": "/rk-and-santosh/static/js/main.3c2a6895.chunk.js"
+  },
+  {
+    "revision": "45790de0241d79fc4c1c",
+    "url": "/rk-and-santosh/static/js/runtime-main.06daedc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rk-and-santosh/static/media/logo.5d5d9eef.svg"
+  }
+]);
